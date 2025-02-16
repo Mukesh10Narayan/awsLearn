baf50c03da9a42fec5fb571c84ae88056def88da
Mukesh Mulpuru <MulpuruMukeshN@JohnDeere.com>
2025-02-16 11:52:55 +0530

    initialize Spring Boot project with basic structure and configuration

---
+++ src/main/java/com/it/awsLearn/AwsLearnApplication.java
@@ -0,0 +1,14 @@
+package com.it.awsLearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsLearnApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
